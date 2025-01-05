6e3774f12c72c5d2694277d8240c35488eb624a2
Konrad <kondykox@gmail.com>
2025-01-05 23:00:04 +0100

    nagłówek

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--clr-primary)",
+        secondary: "var(--clr-secondary)",
+        header: "var(--clr-header)",
+        akcent: "var(--clr-akcent)",
+        button: "var(--clr-button)",
+      },
+      boxShadow: {
+        "inner-button": "inset 0 0 1rem var(--clr-button)",
+        "inner-header": "inset 0 0 1rem var(--clr-header)",
+      },
+    },
   },
   plugins: [],
 };
