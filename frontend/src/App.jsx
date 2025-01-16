e7470edb56031d120fc7bcb8848c2c88af0f67f4
Konrad <kondykox@gmail.com>
2025-01-16 17:59:59 +0100

    stopka

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import ItemList from "./components/ItemList";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <main className="flex flex-col justify-center items-center gap-8 pb-10 w-full">
+    <main className="flex flex-col justify-center items-center gap-8 w-full">
       <Header />
       <ItemList />
+      <Footer />
     </main>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import { LinkIcon } from "@heroicons/react/24/outline";
+
+const Footer = () => {
+  return (
+    <div className="overline-custom w-full flex flex-col justify-center items-center text-center px-2 my-4">
+      <h6 className="text-2xl">
+        <a
+          href="https://github.com/KondyKox/FlaszkoPedia"
+          target="blank"
+          className="flex justify-center items-center gap-2 transition-colors duration-300 ease-in-out hover:text-button"
+        >
+          FlaszkoPedia
+          <LinkIcon className="w-6 h-6" />
+        </a>
+      </h6>
+      <span className="font-bold text-slate-400">
+        Od lat najlepsza porównywarka cen wódki w Polsce
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
