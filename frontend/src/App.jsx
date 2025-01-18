8d8a9b1f0476ae08d3e303adc493f8e08eeece54
Konrad <kondykox@gmail.com>
2025-01-18 22:16:56 +0100

    strona główna

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Vodkas from "./pages/Vodkas";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
       <Navbar />
       <main className="flex flex-col justify-center items-center gap-8 w-full">
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path="/" element={<Home />} />
           <Route path="/vodkas" element={<Vodkas />} />
         </Routes>
         <Footer />
