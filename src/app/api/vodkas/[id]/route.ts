7527fd5fc60a4bca2ceaa4e40b275eb9c5fccc76
Konrad <kondykox@gmail.com>
2025-07-27 15:12:51 +0200

    fixed params type (again)

--- src/app/api/vodkas/[id]/route.ts
+++ src/app/api/vodkas/[id]/route.ts
@@ -5,28 +5,36 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   await connectToDatabase();
 
-  const { id } = await params;
+  const { id } = context.params;
 
   if (!id)
     return NextResponse.json({ error: "No ID in params." }, { status: 400 });
 
-  const vodka = await Vodka.findById(id);
+  try {
+    const vodka = await Vodka.findById(id);
 
-  if (!vodka)
-    return NextResponse.json({ error: "Vodka not found." }, { status: 404 });
+    if (!vodka) {
+      return NextResponse.json({ error: "Vodka not found." }, { status: 404 });
+    }
 
-  console.log(`Request for vodka with ID: ${id}`);
+    console.log(`Request for vodka with ID: ${id}`);
 
-  // --- Vodka description from Wikipedia ---
-  // TODO: Uncomment for vodka description from Wikipedia
-  // const wikiDescription = await getWikiDescription(vodka.name);
-  // const vodkaDescription = vodka.description || "Brak opisu wódki. :(";
+    // --- Vodka description from Wikipedia ---
+    // TODO: Uncomment for vodka description from Wikipedia
+    // const wikiDescription = await getWikiDescription(vodka.name);
+    // const vodkaDescription = vodka.description || "Brak opisu wódki. :(";
+    // const description = `${vodkaDescription}<br/><br/><span class='font-bold'>Opis z Wikipedii:</span><br/>${wikiDescription}`;
 
-  // const description = `${vodkaDescription}<br/><br/><span class='font-bold'>Opis z Wikipedii:</span><br/>${wikiDescription}`;
-
-  return NextResponse.json({ ...vodka.toObject() /* description */ });
+    return NextResponse.json({ ...vodka.toObject() /* description */ });
+  } catch (error) {
+    console.error("Error fetching vodka:", error);
+    return NextResponse.json(
+      { error: "An error occurred while fetching the vodka." },
+      { status: 500 }
+    );
+  }
 }
